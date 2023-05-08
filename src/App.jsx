5379848303121c131ba535bb45fdc17d05e571f5
Leander Christensen <christensenleander@gmail.com>
2023-05-08 20:54:02 +0200

    Added deck comparison

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,67 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { decode } from "deckstrings";
+import { decode } from 'deckstrings';
 
 var cards;
 
-const deckOneString = decode("AAECAfHhBALLpQXMpQUOlrcE9eMEguQEk+QE2fEEsvcEtvcEtIAFk4EFkpMFoJkFopkFrqEFnqoFAA==");
-const deckTwoString = decode("AAECAfHhBALLpQXMpQUOlrcE9eMEguQEk+QE2fEEsvcEtvcEtIAFk4EFkpMFoJkFopkFrqEFnqoFAA==");
+const deckOneString = decode(
+	'AAECAfHhBALLpQXMpQUOlrcE9eMEguQEk+QE2fEEsvcEtvcEtIAFk4EFkpMFoJkFopkFrqEFnqoFAA=='
+);
+const deckTwoString = decode(
+	'AAECAfHhBASpgAW0gAXLpQXMpQUNlrcE9eMEguQEk+QEh/YEtvcEk4EFkpMFoJkFopkFrqEFnqoF+PkFAA=='
+);
 
 async function getCards() {
-  const options = {
-    hostname: 'api.hearthstonejson.com',
-    path: '/v1/latest/enUS/cards.collectible.json',
-    method: 'GET'
-  };
-  async function fetchData(url) {
-    const response = await fetch(url);
-    if (response.status === 302) {
-      const newLocation = response.headers.get("location");
-      return fetchData(newLocation);
-    } else {
-      const data = await response.json();
-      return data;
-    }
-  }
-  try {
-    const url = `https://${options.hostname}${options.path}`;
-    cards = await fetchData(url);
-    return cards;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
+	const options = {
+		hostname: 'api.hearthstonejson.com',
+		path: '/v1/latest/enUS/cards.collectible.json',
+		method: 'GET',
+	};
+	async function fetchData(url) {
+		const response = await fetch(url);
+		if (response.status === 302) {
+			const newLocation = response.headers.get('location');
+			return fetchData(newLocation);
+		} else {
+			const data = await response.json();
+			return data;
+		}
+	}
+	try {
+		const url = `https://${options.hostname}${options.path}`;
+		cards = await fetchData(url);
+		return cards;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
 }
 
 function compareDecks(deckOne, deckTwo) {
-	return deckOne.map(cardOne => {
-		const cardTwo = deckTwo.find(c => c.dbfId === cardOne.dbfId);
+	return deckOne.map((cardOne) => {
+		const cardTwo = deckTwo.find((c) => c.dbfId === cardOne.dbfId);
 		if (!cardTwo) {
-			return { ...cardOne, comparison: 'not-in-deck' };
+			return { ...cardOne, comparison: 0 };
 		}
 		if (cardOne.count === cardTwo.count) {
-			return { ...cardOne, comparison: 'same-count' };
+			return { ...cardOne, comparison: 1 };
 		}
-		return { ...cardOne, comparison: 'different-count' };
+		return { ...cardOne, comparison: 2 };
 	});
 }
 
 function App() {
 	const [deckOne, setDeckOne] = useState([]);
 	const [deckTwo, setDeckTwo] = useState([]);
+	const [deckOneComparison, setDeckOneComparison] = useState([]);
+	const [deckTwoComparison, setDeckTwoComparison] = useState([]);
+
+	useEffect(() => {
+		if (deckOne.length > 0 && deckTwo.length > 0) {
+			setDeckOneComparison(compareDecks(deckOne, deckTwo));
+			setDeckTwoComparison(compareDecks(deckTwo, deckOne));
+		}
+	}, [deckOne, deckTwo]);
 
 	useEffect(() => {
 		async function fetchDataAndInitialize() {
@@ -56,52 +69,70 @@ function App() {
 			var deckOne = [];
 			var deckTwo = [];
 			for (let i = 0; i < deckOneString.cards.length; i++) {
-				let card = cards.find(c => c.dbfId === deckOneString.cards[i][0]); // Find the card object with the matching id
-				card["count"] = deckOneString.cards[i][1];
-				deckOne.push(card);
+				let card = cards.find((c) => c.dbfId === deckOneString.cards[i][0]);
+				let newCard = { ...card, count: deckOneString.cards[i][1] };
+				deckOne.push(newCard);
 			}
 			for (let i = 0; i < deckTwoString.cards.length; i++) {
-				let card = cards.find(c => c.dbfId === deckTwoString.cards[i][0]); // Find the card object with the matching id
-				card["count"] = deckTwoString.cards[i][1];
-				deckTwo.push(card);
+				let card = cards.find((c) => c.dbfId === deckTwoString.cards[i][0]);
+				let newCard = { ...card, count: deckTwoString.cards[i][1] };
+				deckTwo.push(newCard);
 			}
+			//console.log(JSON.stringify(deckOne, null, 2));
 			deckOne.sort((a, b) => a.cost - b.cost);
 			deckTwo.sort((a, b) => a.cost - b.cost);
-			for (let i = 0; i < deckOne.length; i++) {
-				console.log("hello");
-			}
+			//const deckOneComparison = compareDecks(deckOne, deckTwo);
 			setDeckOne(deckOne);
 			setDeckTwo(deckTwo);
 		}
-
 		fetchDataAndInitialize();
 	}, []);
-  return (
-    <div id="main">
-      <table id="deckOne">
-        <tbody>
-          {deckOne.map((item, index) => (
-            <tr key={index}>
-              <td>{item.cost}</td>
-              <td>{item.name}</td>
-              <td>{item.count}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-	<table id="deckTwo">
-        <tbody>
-          {deckTwo.map((item, index) => (
-            <tr key={index}>
-              <td>{item.cost}</td>
-              <td>{item.name}</td>
-              <td>{item.count}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )
+	return (
+		<div id="main">
+			<table id="deckOne">
+				<tbody>
+					{deckOneComparison.map((item, index) => (
+						<tr
+							key={index}
+							style={{
+								backgroundColor:
+									item.comparison === 0
+									? "rgba(255, 0, 0, 0.5)"
+									: item.comparison === 2
+									? "rgba(255, 165, 0, 0.5)"
+									: "transparent",
+							}}
+						>
+							<td>{item.cost}</td>
+							<td>{item.name}</td>
+							<td>{item.count}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+			<table id="deckTwo">
+				<tbody>
+					{deckTwoComparison.map((item, index) => (
+						<tr
+							key={index}
+							style={{
+								backgroundColor:
+									item.comparison === 0
+									? "rgba(255, 0, 0, 0.5)"
+									: item.comparison === 2
+									? "rgba(255, 165, 0, 0.5)"
+									: "transparent",
+							}}
+						>
+							<td>{item.cost}</td>
+							<td>{item.name}</td>
+							<td>{item.count}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
-export default App
+export default App;
